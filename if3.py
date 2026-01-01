0390ccceb00e0fa1dcd17e31588dde73e4365103
LawrenceJelleh <jellehlawrence86@gmail.com>
2026-01-01 14:55:51 +0530

    Add files via upload

---
+++ if3.py
@@ -0,0 +1,9 @@
+# Find the greatest of two numbers
+
+a = int(input("Enter first number: "))
+b = int(input("Enter Second number: "))
+
+if a > b:
+    print("A is greater")
+else:
+    print("B is greater")
